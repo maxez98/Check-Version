b0921b26eef71d8cb3ac42b0af9d8c8d3fdfc5a6
maxez98 <114407745+maxez98@users.noreply.github.com>
2024-09-02 23:51:10 +0200

    Add files via upload

---
+++ Check Version Office.ps1
@@ -0,0 +1,14 @@
+﻿forEach ($appId in 'Access', 'Excel', 'Lync', 'OneNote', 'Outlook', 'PowerPoint', 'Publisher', 'Visio', 'Word') {
+
+   $regKey =  "hklm:\Software\Classes\$($appId).application\curVer"
+
+   if (-not (test-path $regKey)) {
+     $version = 'n/a'
+   }
+   else {
+      $defaultValue = (get-item $regKey).getValue('')
+   
+      $version =  $defaultValue -replace '.*\.(\d+)', '$1'
+   }
+   write-output('  {0,-10} {1,3}' -f $appId, $version)
+}
